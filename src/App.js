19aff8d65aca7b3289fde24023937632d939c511
nanifool <luanadarosadasilva920522@gmail.com>
2021-06-03 16:51:57 -0300

    onClick e onChange exemplos

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import {useState} from 'react'
-import './App.css';
+import Listagem from './Listagem'
+import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
+  let banana = 0
   const somaClicks = function (){
     setCount(count+1)
+    banana = banana+1
+    console.log({count, banana})
+  }
+  const subtraiClicks = function() {
+    setCount(count-1)
     console.log(count)
   }
+
   return (
     <div className="App">
+      {count}/{banana} <br />
       <button onClick={somaClicks} >
-        Clique aqui
+        Soma 1
       </button>
+      <button onClick={subtraiClicks} >
+        Diminui 1
+      </button>
+      <Listagem />
     </div>
-  );
+  )
 }
 
 export default App;
